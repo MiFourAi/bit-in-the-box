517e5003664b22bb476eb1e6af4b3a4aa0f133d3
Mike Miles <mike.miles.8341@gmail.com>
2017-12-23 10:24:13 -0500

    refactored market data

--- js/gdax_test.js
+++ js/gdax_test.js
@@ -1,13 +1,22 @@
 const GdaxFeedHandler = require('./gdax_mess.js').GdaxFeedHandler;
 
 
-var gfh = new GdaxFeedHandler(['BTC-USD'], ['level2']);
+var gfh = new GdaxFeedHandler(['BTC-USD'], ['full', 'level2']);
 
-gfh.on('message', function(msg) {
-  var book = msg.orderBook;
+gfh.on('message', function(book) {
+	if (book.md_type !== 'OrderBook') {
+		return;
+	}
+
   var bid1 = book.bids.max();
   var ask1 = book.asks.min();
-  console.log('bid1: (', bid1.qty, ' @ ', bid1.price.toString(), '), ask1: (', ask1.qty, ' @ ', ask1.price.toString(), ')');
+  console.log('bid1: ($', bid1.price.toString(), ', ', bid1.qty, '), ask1: ($', ask1.price.toString(), ', ', ask1.qty , ')');
 });
+
+// gfh.on('message', function(msg) {
+// 	if (msg.md_type === 'Order' || msg.md_type === 'Trade') {
+// 		console.log(msg);
+// 	}
+// });
 
 gfh.start();
