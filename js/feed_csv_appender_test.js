91ad2eb735e564943750d181e31a5a09616fbaad
FroxieYe <kittyhawkraptor@gmail.com>
2017-12-23 23:38:44 -0800

    add csv appender

---
+++ js/feed_csv_appender_test.js
@@ -0,0 +1,13 @@
+const FeedCsvAppender = require('./feed_csv_appender').FeedCsvAppender;
+
+var appender = new FeedCsvAppender('btcusd', 'bitstamp', 'order_book', 300);
+msg = {
+    exchange: 'bitstamp',
+    productID: 'btcusd',
+    exchangeTimestamp: 1318781876,
+    timestamp: (new Date().getTime()),
+    sequenceNo: 0,
+    bids: [{'price': 1, 'qty': 2}, {'price': 2, 'qty': 3}],
+    asks: [{'price': 2, 'qty': 3}, {'price': 1, 'qty': 4}],
+};
+appender.append(msg);
--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,24 @@
 const num = require('num');
+const moment = require('moment');
+const shell = require('shelljs');
 
 var num2f = function(f) {
 	return num(f).set_precision(2);
 }
 
+var epochToTime = function(epochts) {
+	return moment(epochts).format('YYYYMMDDThhmmss');
+}
+
+var epochToDate = function(epochts) {
+	return moment(epochts).format('YYYYMMDD');
+}
+
+var makeDir = function(dir) {
+    shell.mkdir('-p', dir);
+}
+
 module.exports.pricef = num2f;
+module.exports.makeDir = makeDir;
+module.exports.epochToDate = epochToDate;
+module.exports.epochToTime = epochToTime;
