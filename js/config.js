44381af41999c710fd53346a23d9dda1ab1cc7dc
FroxieYe <kittyhawkraptor@gmail.com>
2017-12-24 16:23:38 -0800

    move products and channels to config

--- js/config.js
+++ js/config.js
@@ -1,4 +1,9 @@
 var config = {};
 config.basePath = '/Users/wenshuaiye/Dropbox/bitcoin_files/sample_data';
+config.bitstamp = {
+	products = ['btcusd'],
+	channels = ['live_trades', 'order_book', 'live_orders'],
+}
+cnofig.bitfinex = {}
 
 module.exports.config = config;
