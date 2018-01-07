e74419881854ed3073da9838e6f4e1a3d97ade8b
Mike Miles <mike.miles.8341@gmail.com>
2018-01-07 09:39:04 -0800

    log timestamp in run_process.sh

--- tools/run_process.sh
+++ tools/run_process.sh
@@ -5,23 +5,28 @@ PID="null"
 RUNNING="true"
 LOG='run_process_log'
 
+
 function sigint_handler() {
   if [ "$PID" != "null" ]; then
     echo "kill -15 $PID"
     kill -15 $PID
   fi
   RUNNING="false"
-  echo "Done" >> $LOG
+  ts=$(date +"%Y%m%d-%T")
+  echo "[$ts] Done" >> $LOG
 }
 # install the SIGINT handler for ctrl+c
 trap 'sigint_handler' 2
 
+rm -f $LOG
 touch $LOG
+
 i=0
 while [ $RUNNING == "true" ]; do
   $CMD &
   PID=$!
-  echo "Running $i, PID=$PID, CMD=$CMD" >> $LOG
+  ts=$(date +"%Y%m%d-%T")
+  echo "[$ts] Running $i, PID=$PID, CMD=$CMD" >> $LOG
   # ps
   wait $PID
   # echo "done running $i"
