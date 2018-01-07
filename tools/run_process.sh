46cdb1a71cc002dbfcd41f063def63e1962a2b8f
Mike Miles <mike.miles.8341@gmail.com>
2018-01-06 22:19:02 -0800

    run_process.sh output to log

--- tools/run_process.sh
+++ tools/run_process.sh
@@ -3,6 +3,7 @@
 CMD="node ../js/gdax_subscription_app.js"
 PID="null"
 RUNNING="true"
+LOG='run_process_log'
 
 function sigint_handler() {
   if [ "$PID" != "null" ]; then
@@ -10,14 +11,17 @@ function sigint_handler() {
     kill -15 $PID
   fi
   RUNNING="false"
+  echo "Done" >> $LOG
 }
+# install the SIGINT handler for ctrl+c
 trap 'sigint_handler' 2
 
+touch $LOG
 i=0
 while [ $RUNNING == "true" ]; do
   $CMD &
   PID=$!
-  echo "Running $i, PID=$PID, CMD=$CMD"
+  echo "Running $i, PID=$PID, CMD=$CMD" >> $LOG
   # ps
   wait $PID
   # echo "done running $i"
