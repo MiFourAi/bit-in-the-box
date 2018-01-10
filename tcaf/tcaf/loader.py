5b2a983eb8159365f141e05df816acc607040747
FroxieYe <kittyhawkraptor@gmail.com>
2018-01-09 23:15:21 -0800

    a hacky prototype subscriber

--- tcaf/tcaf/loader.py
+++ tcaf/tcaf/loader.py
@@ -3,8 +3,13 @@ import csv
 import itertools
 import pandas as pd
 import numpy as np
+from collections import namedtuple
+from collections import defaultdict
+from heapq import *
 
 import utils
+
+
 
 global __ROOT_PATH
 __ROOT_PATH = '/Users/wenshuaiye/Kaggle/bitcoin/data'
@@ -16,28 +21,86 @@ def set_path(path):
 def get_path():
 	return __ROOT_PATH
 
-class Poller(object):
-
-	def __init__(self, starttime, endtime, ):
-		pass
-	pass
 
 class Subscriber(object):
-	pass
+
+	def __init__(self, starttime, endtime, exchanges, products, table):
+		self._starttime = starttime
+		self._endtime = endtime
+		self._exchanges = exchanges
+		self._products = products
+		self._startdate = starttime.split('T')[0]
+		self._enddate = endtime.split('T')[0]
+		self._table = table
+
+
+	def _walk_paths(self):
+		path_dict = defaultdict(list)
+		for exchange in self._exchanges:
+			for product in self._products:
+				path = os.path.join(get_path(), self._table, exchange, product)
+				if os.path.exists(path):
+					for date in sorted(os.listdir(path)):
+						if date < self._startdate:
+							continue
+						if date > self._enddate:
+							break
+						path_dict[(exchange, product)].append(os.path.join(
+							path, date))
+
+		return path_dict
+
+
+	def process(self):
+		path_dict = self._walk_paths()
+
+		subscribers = {}
+		data_stream = []
+		for k, paths in path_dict.iteritems():
+			subscribers[k] = CsvDriver(
+				self._starttime,
+				self._endtime,
+				paths.pop(0),
+				self._table).poll()
+
+		while len(subscribers) > 0:
+			deleted_subscribers = set()
+			for key, subscriber in subscribers.iteritems():
+				try:
+					entry = subscriber.next()
+				except StopIteration:
+					if len(path_dict[key]) > 0:
+						subscribers[key] = CsvDriver(
+							self._starttime,
+							self._endtime,
+							path_dict[key].pop(0),
+							self._table).poll()
+					else:
+						deleted_subscribers.add(key)
+						continue
+				heappush(data_stream, (getattr(entry, 'timestamp'), entry))
+			for key in deleted_subscribers:
+				subscribers.pop(key)
+
+			if len(data_stream) > 0:
+				yield heappop(data_stream)
+
 
 class Subscription(object):
 	pass
 
-class CsvQuery(object):
+class CsvDriver(object):
 
-	def __init__(self, starttime, endtime, path):
+	def __init__(self, starttime, endtime, path, table_name):
 		self._starttime = starttime
 		self._endtime = endtime
 		self._path = path
+		self._table_name = table_name
 
 
 	def _ls_file(self):
-		all_files = filter(lambda x: x.endswith('.csv'), sorted(os.listdir(self._path)))
+		all_files = filter(
+			lambda x: x.endswith('.csv'), sorted(os.listdir(self._path)))
 		start_idx = self._searchsorted(all_files, self._starttime)
 		end_idx = self._searchsorted(all_files, self._endtime)
 		return all_files[start_idx:end_idx + 1]
@@ -80,6 +143,18 @@ class CsvQuery(object):
 		return data
 
 
+	def poll(self):
+		all_files = self._ls_file()
+		for i in xrange(len(all_files)):
+			csvfile = os.path.join(self._path, all_files[i])
+			with open(csvfile, 'rb') as csvf:
+				reader = csv.reader(csvf, delimiter=',')
+				header = next(reader)
+				table = namedtuple(self._table_name, header)
+				for row in reader:
+					yield table._make(row)
+
+
 class Query(object):
 
 	def __init__(self, starttime, endtime, exchanges, products):
@@ -106,8 +181,8 @@ class Query(object):
 					yield os.path.join(path, date)
 
 	def _create_query_driver(self, table):
-		return [CsvQuery(self._starttime, self._endtime, path) for path in
-			self._walk_paths(table)]
+		return [CsvDriver(self._starttime, self._endtime, path, table) for
+			path in self._walk_paths(table)]
 
 	def query(self, table):
 		query_drivers = self._create_query_driver(table)
@@ -118,10 +193,26 @@ class Query(object):
 		return data if data.shape[0] == 0 else data.sort_values('timestamp')
 
 
+def main():
+	#query_object = Query(
+	#	'20180105T000000', '20180106T000000', ['bitstamp'], ['btcusd'])
+	#print(query_object.query('Order').shape)
+	#test_path = \
+	#	"/Users/wenshuaiye/Kaggle/bitcoin/data/Order/bitstamp/btcusd/20180107"
+	#csv_driver = CsvDriver(
+	#	'20180108T000000', '20180108T00300', test_path, 'Order')
+	#print(csv_driver.stream())
+	query_object = Subscriber(
+		'20180105T000000',
+		'20180105T000100',
+		['bitstamp'],
+		['btcusd'],
+		'Order').process()
+
+	print(query_object.next())
+	print(query_object.next())
+	print(query_object.next())
+
+
 if __name__ == '__main__':
-	query_object = Query(
-		'20180105T000000', '20180106T000000', ['bitstamp'], ['btcusd'])
-	print query_object.query('Order').shape
-
-
-
+	main()
